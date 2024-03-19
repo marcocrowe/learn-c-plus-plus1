279c316d7490f5ff85959942ff66593b0fa759df
Adam Nix <150264616+realadamnix@users.noreply.github.com>
2024-03-19 09:07:42 +0000

    FunctionsGroupProject V2

--- FunctionsGroupProject/FunctionsGroupProject.cpp
+++ FunctionsGroupProject/FunctionsGroupProject.cpp
@@ -1,37 +1,136 @@
-
 #include <iostream>
 using namespace std;
 
+void MenuChoices(int array[], int size);
+void populateArray(int array[], int size);
+void UserChoiceInput(int array[], int size);
+void DisplayArray(int array[], int size);
+void TotalArray(int array[], int size);
 
-void MenuSystem()
+
+void MenuChoices(int array[], int size)
 {
-	cout << "---------------------------------------------------" << endl;
-	cout << "1. Display the Numbers in the array               |" << endl;
-	cout << "2. get total of the array                         |" << endl;
-	cout << "3. Get the Average of the array                   |" << endl;
-	cout << "4. Get the Largest number in the array            |" << endl;
-	cout << "5. Get the Smallest number in the array           |" << endl;
-	cout << "6. Scale up by one                                |" << endl;
-	cout << "7. reverse the array                              |" << endl;
-	cout << "8. Zero Base                                      |" << endl;
-	cout << "9. remove a number form the array                 |" << endl;
-	cout << "10. sort the array form ascending order           |" << endl;
-	cout << "11. Exit                                          |" << endl;
-	cout << "---------------------------------------------------" << endl;
+	cout << "--------------------------------------------------" << endl;
+	cout << "select 1 to Display the Numbers in the array      |" << endl;
+	cout << "select 2 to get total of the array                |" << endl;
+	cout << "select 3 to Get the Average of the array          |" << endl;
+	cout << "select 4 to Get the Largest number in the array   |" << endl;
+	cout << "select 5 to Get the Smallest number in the array  |" << endl;
+	cout << "select 6 to Scale up by one                       |" << endl;
+	cout << "select 7 to reverse the array                     |" << endl;
+	cout << "select 8 to Zero Base                             |" << endl;
+	cout << "select 9 to remove a number form the array        |" << endl;
+	cout << "select 1 to sort the array form ascending order   |" << endl;
+	cout << "select 1 to Exit                                  |" << endl;
+	cout << "--------------------------------------------------" << endl;
 }
+
+void populateArray(int array[], int size)
+{
+	cout << "Enter 5 numbers to populate the array: " << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> array[i];
+	}
+}
+
+
+void UserChoiceInput(int array[], int size)
+{
+	int Menu;
+	cout << "Enter your choice: ";
+	cin >> Menu;
+
+	switch (Menu)
+	{
+	case 1:
+		DisplayArray(array, size);
+		break;
+
+	case 2:
+		TotalArray(array, size);
+		break;
+
+	case 3:
+
+		break;
+
+	case 4:
+
+		break;
+
+	case 5:
+
+		break;
+
+
+	case 6:
+
+		break;
+
+	case 7:
+
+		break;
+
+	case 8:
+
+		break;
+
+	case 9:
+
+		break;
+
+	case 10:
+
+		break;
+
+	case 11:
+
+		exit(0);
+
+		break;
+
+	default:
+		cout << "Invalid choice" << endl;
+		break;
+	}
+}
+
+void DisplayArray(int array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+	}
+	cout << endl;
+}
+
+
+void TotalArray(int array[], int size)
+{
+	int total = 0;
+	for (int i = 0; i < size; i++)
+	{
+		total += array[i];
+	}
+	cout << "The total of the array is: " << total << endl;
+}
+
+
+
+
+
 
 
 
 int main()
 {
-	int populateArray[50];
-	cout << "Enter 50 numbers to populate the array: " << endl;
-	for (int i = 0; i < 50; i++)
-	{
-		cin >> populateArray[i];
-	}
+	int array[5];
+	int size = 5;
 
-	MenuSystem();
+	populateArray(array, size);
+	MenuChoices(array, size);
+	UserChoiceInput(array, size);
 
+	return 0;
 }
-
